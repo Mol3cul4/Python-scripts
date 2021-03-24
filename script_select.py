880ed9a24b93b98592f57287b95e9226a93350f3
Mol3cul4RNA <44534742+Mol3cul4@users.noreply.github.com>
2021-03-24 16:12:29 -0300

    Add files via upload

---
+++ script_select.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import csv
+from Bio import SeqIO
+
+#os.popen('cp /home/IEC/kennypinheiro/Downloads/Teste/pasta_contigs/*.fa /home/IEC/kennypinheiro/Downloads/Teste/')
+
+tsv_file = open("virus.tsv") #Path for TSV table. Changes in 1 column.
+
+read_tsv = csv.reader(tsv_file, delimiter = "\t")
+
+lista_contigs = []
+lista_samples = []
+
+for row in read_tsv:
+	lista_contigs.append(row[1])
+	lista_samples.append(row[0])
+
+zip_iterator = zip(lista_contigs, lista_samples)
+
+a_dictionary = dict(zip_iterator)
+
+#Directory with assembled contigs fasta
+directory = "/home/IEC/kennypinheiro/Downloads/Teste/pasta_contigs" # <-- change here
+
+#Create tmp files
+#os.popen('cp /home/IEC/kennypinheiro/Downloads/Teste/pasta_contigs/*.fa /home/IEC/kennypinheiro/Downloads/Teste/') # <-- change here
+
+files_contigs = os.listdir(directory)
+
+qtd_files_contigs = len(files_contigs)
+
+for i in range(qtd_files_contigs):
+	for j in a_dictionary:
+		if a_dictionary[j] == files_contigs[i].split('.')[0]:
+			for seq_record in SeqIO.parse(files_contigs[i], "fasta"):
+				if seq_record.id == j:
+					original_stdout = sys.stdout
+					with open(files_contigs[i].split('.')[0]+"_"+seq_record.id+".fasta", 'w') as f:
+						sys.stdout = f
+						print(">"+files_contigs[i].split('.')[0]+" "+seq_record.id)
+						print(seq_record.seq)
+						sys.stdout = original_stdout
+
+#Remove tmp files
+os.popen('rm /home/IEC/kennypinheiro/Downloads/Teste/*.fa') # <-- change here
+
+#Moving results to a new directory
+os.popen('mkdir /home/IEC/kennypinheiro/Downloads/Teste/results/') # <-- change here
+os.popen('mv /home/IEC/kennypinheiro/Downloads/Teste/*.fasta /home/IEC/kennypinheiro/Downloads/Teste/results/') # <-- change here
