4322ac65d7dba54b9d3a10ed614d0921e9dd2d16
Mol3cul4RNA <44534742+Mol3cul4@users.noreply.github.com>
2021-04-07 23:14:13 -0300

    Add files via upload

---
+++ script_turtle.py
@@ -0,0 +1,21 @@
+import turtle
+import math
+bob = turtle.Turtle() 
+
+def square(t, length):
+	for i in range(4):
+		t.fd(length)
+		t.lt(90)
+
+def polygon(t, length, n):
+	for i in range(n):
+		t.fd(length)
+		t.lt(360 / n)
+
+def circle(t, r):
+	circumference = 2*math.pi*r
+	n = 50 
+	length = circumference / n
+	polygon(t, length, n)
+
+circle(bob, 50)
